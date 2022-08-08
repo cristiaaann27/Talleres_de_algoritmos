84e31215d65e27e5e4e47d4bf7845f94b13e85c8
Cristian Steven Ñustes Hernandez <cnustes78568@universidadean.edu.co>
2022-08-07 23:02:32 -0500

    ejercicio uno

---
+++ Taller_estructuras_de_control_secuenciales/ejercicio_uno.py
@@ -0,0 +1,10 @@
+# Entradas
+edad_uno = int(input("Digite la primera edad: "))
+edad_dos = int(input("Digite la segunda edad: "))
+edad_tres = int(input("Digite la tercera edad: "))
+
+# Caja negra
+promedio = (edad_uno + edad_dos + edad_tres)/3
+
+#Salida 
+print(f"Promedio: {promedio}")
